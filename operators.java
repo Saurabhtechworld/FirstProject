4632fb6a869ef87fe10524c16f79498a5264d9cd
Saurabh Pandey <128401883+Saurabhtechworld@users.noreply.github.com>
2026-06-03 20:53:59 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,16 @@
+package basic2;
+
+public class operators {
+   public static void main(String[] args) {
+//        int firstNumber = 10;
+//        int secondNumber = 20;
+//        double c = a/(double)b;
+//       System.out.println(a+b);
+//       System.out.println(firstNumber < secondNumber);
+
+       boolean firstExpression = false;
+       boolean secondExpression = false;
+//       System.out.println(firstExpression && secondExpression);
+       System.out.println(firstExpression || secondExpression);
+   }
+}
